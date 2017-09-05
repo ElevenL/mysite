263982eeab6697a41c2efab90f10e09317619234
Eleven <lhqFH2818>
2017-09-05 21:15:48 +0800

    添加表单

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import *
 from django.contrib import admin
+from django.views.generic import RedirectView
 admin.autodiscover()
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/blog/')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls')),
 ]
