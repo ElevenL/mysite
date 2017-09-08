b0fcc93405794e8a4cbdec0a1b1c52ac47bb3d14
Eleven <lhqFH2818>
2017-09-08 19:45:14 +0800

    create admin of reading

--- reading/admin.py
+++ reading/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from reading import models
 
 # Register your models here.
+
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author')
+
+admin.site.register(models.BookInfo, BookInfoAdmin)
