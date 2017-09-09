e21e4af30e341d53577b987d8f1f3b7424f85f40
Eleven <lhqFH2818>
2017-09-09 19:22:00 +0800

    modify base.html in reading

--- reading/views.py
+++ reading/views.py
@@ -11,11 +11,40 @@ from django.views.decorators.csrf import csrf_protect
 # @csrf_protect
 def index(request):
     page_pernum = 2
-    page = 1
     allBookCounts = BookInfo.objects.count()
     max_page = int(allBookCounts / page_pernum)
-    books = BookInfo.objects.all()[0:page_pernum]
-    return render(request, 'index.html', {'books': books, 'cur_page': page, 'max_page': max_page})
+    page = int(request.GET.get('page', '1'))
+    if page > max_page:
+        page = max_page
+    elif page < 1:
+        page = 1
+    start_id = (page - 1) * page_pernum
+    end_id = start_id + page_pernum
+    books = BookInfo.objects.all()[start_id:end_id]
+    last_page = 5
+    if page <= 3:
+        last_page =5
+    elif page >= (max_page - 2):
+        last_page = max_page
+    else:
+        last_page = page + 2
+    if last_page > max_page:
+        last_page = max_page
+    page_list = range(last_page - 4, last_page + 1)
+    if page_list[0] == 1:
+        p_page = 1
+    else:
+        p_page = page_list[0] - 1
+    if page_list[-1] == max_page:
+        n_page = max_page
+    else:
+        n_page = page_list[-1] + 1
+    return render(request, 'index.html',
+                  {'books': books,
+                   'cur_page': str(page),
+                   'page_list': page_list,
+                   'p_page': str(p_page),
+                   'n_page': str(n_page)})
 
 def get_page(request):
     page_pernum = 2
