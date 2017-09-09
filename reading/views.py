c826751cb0d8e6deb508afaa3777542d09dc8709
Eleven <lhqFH2818>
2017-09-09 20:32:42 +0800

    modify base.html in reading

--- reading/views.py
+++ reading/views.py
@@ -7,14 +7,17 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
+def make_pages():
+    pass
+
 
 # @csrf_protect
 def index(request):
     page_pernum = 2
     allBookCounts = BookInfo.objects.count()
     max_page = int(allBookCounts / page_pernum)
-    if max_page < 5:
-        max_page = 5
+    # if max_page < 5:
+    #     max_page = 5
     page = int(request.GET.get('page', '1'))
     if page > max_page:
         page = max_page
@@ -32,7 +35,10 @@ def index(request):
         last_page = page + 2
     if last_page > max_page:
         last_page = max_page
-    page_list = range(last_page - 4, last_page + 1)
+    if last_page >= 5:
+        page_list = range(last_page - 4, last_page + 1)
+    else:
+        page_list = range(1, last_page + 1)
     if page_list[0] == 1:
         p_page = 1
     else:
@@ -48,10 +54,14 @@ def index(request):
                    'p_page': str(p_page),
                    'n_page': str(n_page)})
 
-def get_page(request):
+def search(request):
     page_pernum = 2
-    page = int(request.GET.get('page', '1'))
-    allBookCounts = BookInfo.objects.count()
+    name_kw = int(request.GET.get('q', 'all'))
+    if name_kw == 'all':
+        return index(request)
+    else:
+        search_content = BookInfo.objects.filter(name__icontains=name_kw)
+    allBookCounts = len(search_content)
     max_page = int(allBookCounts / page_pernum)
     start_id = (page - 1) * page_pernum
     end_id = start_id + page_pernum
