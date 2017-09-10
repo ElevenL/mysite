2de4524ca11bf46a856cd981865bf01db7c58def
Eleven <lhqFH2818>
2017-09-10 09:13:05 +0800

    add search in reading

--- reading/views.py
+++ reading/views.py
@@ -92,7 +92,7 @@ def search(request):
 
 def download(request):
     # do something...
-    file_name = str(request.get_full_path()).split('/')[-1]
+    file_name = urllib.unquote(str(request.get_full_path()).split('/')[-1])
     file_path = '/root/book/download/' + file_name
     def file_iterator(file_name, chunk_size=512):
         with open(file_name) as f:
